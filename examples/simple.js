6956b5d6cd0ef1b57138c1021e536861550b5ab3
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-23 13:32:52 +1100

    Create an image element with the captured image

--- examples/simple.js
+++ examples/simple.js
@@ -1,13 +1,15 @@
 var snapstream = require('..');
 var getUserMedia = require('getusermedia');
 var kgo = require('kgo');
+var h = require('hyperscript');
 
 kgo({
-  constraints: { video: true, audio: true }
+  constraints: { video: true, audio: false }
 })
 ('capture', ['constraints'], getUserMedia)
 ('snap', ['capture'], snapstream)
 ('processImage', ['snap'], function(imageData) {
   console.log('got image data: ', imageData, imageData.length);
+  document.body.appendChild(h('img', { src: imageData }));
 })
 .on('error', console.error.bind(console));
